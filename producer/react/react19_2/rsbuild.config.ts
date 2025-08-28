c833863620379f1df262ee6972e306084ae84a4c
Paul Ganster <paul.ganster@dccs.eu>
2025-08-28 16:59:33 +0200

    Add top level share scope to React 19 producers

--- producer/react/react19_2/rsbuild.config.ts
+++ producer/react/react19_2/rsbuild.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         './UiButton': './src/export-uibutton',
       },
       shareStrategy: 'loaded-first',
+      shareScope: 'react19',
       shared: {
         react: {
           singleton: true,
